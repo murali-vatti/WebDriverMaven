cadf0e3ab23eeba0723277251026e846c685ee59
murali vatti <muralivatti@gmail.com>
2022-01-06 15:08:02 +0530

    config file added and LoginTest.java modified

--- src/test/java/com/murali/LoginTest.java
+++ src/test/java/com/murali/LoginTest.java
@@ -1,8 +1,15 @@
 package com.murali;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -13,29 +20,57 @@ public class LoginTest {
 	public static WebDriver driver;
 	
 	@BeforeTest
-	public void setUp() {
+	public void setUp() throws IOException {
 		
-		WebDriverManager.chromedriver().setup();		
-		driver = new ChromeDriver();	
+				
+		Properties OR = new Properties();
+		FileInputStream fis;
+		String browser;
+		fis = new FileInputStream("E:\\GitWebDriverMaven\\webdriverMavenGit\\src\\test\\java\\com\\murali\\config.properties");
+				
+		OR.load(fis);		
+
+		OR.getProperty("browser");
+		
+		if (System.getenv("browser") !=null && !System.getenv("browser").isEmpty()) {
+			
+			browser =  System.getenv("browser");
+		}
+		else {
+			browser = OR.getProperty("browser");
+		}		
+		
+		OR.setProperty("browser", browser);
+		
+		
+		
+		if(browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();				
+		}else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}else if (browser.equals("edge")) {
+			
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}		
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10) );
 		
 	}
 	
 	@Test
-	public void openWebsite() {
+	public void openWebsite() {		
+		
+		//test.log(LogStatus.INFO, "Login bank manager");	
 		
 		
-		
-		//test.log(LogStatus.INFO, "Login bank manager");
-		
-
 	
-		driver.get("https://flipkart.com");		
-		driver.close();
-		driver = new ChromeDriver();		
+	
 		driver.get("https://amazon.in");
-		driver.manage().window().maximize();
+	
 
 		
 		
