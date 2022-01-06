d081ed4d170f5f8ae734d4071b60ef86bc4faba8
murali vatti <muralivatti@gmail.com>
2022-01-06 11:22:00 +0530

    second checkin

--- src/test/java/com/murali/LoginTest.java
+++ src/test/java/com/murali/LoginTest.java
@@ -1,14 +1,11 @@
 package com.murali;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
@@ -28,7 +25,13 @@ public class LoginTest {
 	@Test
 	public void openWebsite() {
 		
-		driver.get("https://flipkart.com");
+		
+		
+		//test.log(LogStatus.INFO, "Login bank manager");
+		
+
+	
+		driver.get("https://flipkart.com");		
 		driver.close();
 		driver = new ChromeDriver();		
 		driver.get("https://amazon.in");
